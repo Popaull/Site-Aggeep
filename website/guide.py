00ba7b71e52311f9590ebbaedf2e3f8731305af7
popaul <skydaz73170@gmail.com>
2023-03-12 18:38:48 +0100

    2

--- website/guide.py
+++ website/guide.py
@@ -4,7 +4,7 @@ from flask import Blueprint , render_template
 
 guide = Blueprint('guide', __name__)
 
-@guide.route('/Guide_de_la_vie_étudiante')
+@guide.route('/Quisommes-nous')
 
 def home():
     return render_template("guide.html")
